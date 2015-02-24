c72d9ab98e52ab9c2395c76c43a9f4cf3160547b
Hayden Goldstien <haydengoldstien@gmail.com>
2015-02-24 09:26:18 -0600

    Begin Django workflow

---
+++ Django/scripty.sh
@@ -0,0 +1,31 @@
+#!/bin/bash=
+verify_python_setup(){
+	echo "VERIFY PYTHON IS INSTALLED AND REPO IS ORGANIZED"
+	source Python/scripty.sh init
+}
+
+installation(){
+	echo "INSTALL DJANGO"
+	pip install django
+}
+
+init(){
+	verify_python_setup
+	installation
+}
+
+init_psql(){
+	echo "INSTALL PSYCOPG2"
+	pip install psycopg2
+}
+
+if [ "$1" = "init" ]; then
+	init
+	if [ "$2" = "with" ]; then
+		if [ "$3" = "psql"]; then
+			init_psql
+		fi
+	fi
+	name=$(echo "${PWD##*/}" | sed 'y/-/_/')
+	django-admin.py startproject $name
+fi
